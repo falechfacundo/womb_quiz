c007d78c8844c9092ce2b16e696127b6e3bd2ccf
Facundo E. Falech <102884142+facufaia@users.noreply.github.com>
2025-02-12 12:38:17 -0300

    few changes

--- .vercel/output/_functions/chunks/supabase_CAfStfkl.mjs
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  "https://tuuuxxfvasbtffoyzent.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1dXV4eGZ2YXNidGZmb3l6ZW50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxNDY1NTEsImV4cCI6MjA1NDcyMjU1MX0.hY3JMZhVn-eUE01U5t8aBTIARn7yqXJzdZxGlu2qAug"
-);
-
-export { supabase as s };
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_D3cs8JO0.mjs';
-export { renderers } from '../renderers.mjs';
